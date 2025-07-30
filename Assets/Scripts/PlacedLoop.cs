8b4f612a5c654299746d0a812daccbe4c2ce9d96
JoeShanahan <iam3djoe@gmail.com>
2025-07-30 23:39:19 +0100

    locking onto loops

--- Assets/Scripts/PlacedLoop.cs
+++ Assets/Scripts/PlacedLoop.cs
@@ -1,18 +1,36 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gmtk2025
 {
     public class PlacedLoop : MonoBehaviour
     {
+        [Serializable]
+        public class ConnectorInfo
+        {
+            public Connector Connector;
+            
+            [Tooltip("Value between 0 and 1, position in loop-space")]
+            public float Offset;
+
+            public PlacedLoop OtherLoop;
+        }
+        
         [SerializeField] private LineRenderer _line;
 
         [SerializeField, Range(0, 20)] private float _radius = 1;
 
+        [SerializeField] private List<Connector> _connectors;
+
+        private CircleCollider2D _collider;
+        
         private const int POINT_COUNT = 32;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
+            _collider = GetComponent<CircleCollider2D>();
             Sync();
         }
 
@@ -22,24 +40,43 @@ namespace Gmtk2025
 
         }
 
+        private const float TAU = Mathf.PI * 2;
+
+        public float Circumference => _radius * TAU;
+
+        // 0 is the absolute top, 0.25 is the absolute right edge, 0.5 is the absolute bottom
+        public float PositionToLoopSpace(Vector3 position)
+        {
+            Vector2 offset = position - transform.position;
+            float angle = Mathf.Atan2(offset.y, offset.x);
+            return -angle / TAU;
+        }
+
+        public Vector3 LoopSpaceToPosition(float loopSpace)
+        {
+            float angle = -loopSpace * TAU;
+            return new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)) * _radius;
+        }
+
         [ContextMenu("Sync!")]
         public void Sync()
         {
-            Vector3 origin = transform.position;
             Vector3[] positions = new Vector3[POINT_COUNT + 1];
 
             for (int i = 0; i <= POINT_COUNT; i++)
             {
-                float angle = ((Mathf.PI * 2) / POINT_COUNT) * i;
+                float angle = (TAU / POINT_COUNT) * i;
 
-                float posX = origin.x + (Mathf.Cos(angle) * _radius);
-                float posY = origin.y + (Mathf.Sin(angle) * _radius);
+                float posX = Mathf.Cos(angle) * _radius;
+                float posY = Mathf.Sin(angle) * _radius;
 
-                positions[i] = new Vector3(posX, posY, origin.z);
+                positions[i] = new Vector3(posX, posY, 0);
             }
 
             _line.positionCount = POINT_COUNT + 1;
             _line.SetPositions(positions);
+
+            GetComponent<CircleCollider2D>().radius = _radius;
         }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,16 +4,74 @@ namespace Gmtk2025
 {
     public class Projectile : MonoBehaviour
     {
+        public bool IsOnLoop => _currentLoop != null;
+
+        // Positive = clockwise
+        private float _speed;
+        
+        private PlacedLoop _currentLoop;
+        private Rigidbody2D _rb;
+        private CircleCollider2D _collider;
+        
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-
+            _rb = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<CircleCollider2D>();
         }
 
-        // Update is called once per frame
+        private void SwapToOnLoop(PlacedLoop loop)
+        {
+            // TODO use the dot product of the current velocity and the tangent
+            _speed = 5;
+            
+            _rb.simulated = false;
+            _currentLoop = loop;
+
+            float loopSpace = loop.PositionToLoopSpace(transform.position);
+            Vector3 newPos = loop.LoopSpaceToPosition(loopSpace);
+            newPos.z = transform.position.z;
+            transform.position = newPos;
+        }
+
+        private void SwapToFree()
+        {
+            _rb.simulated = true;
+        }
+        
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (IsOnLoop)
+                return;
+            
+            if (other is CircleCollider2D otherCircle)
+            {
+                // We need the center of our object to pass the edge of this collider
+                float distance = Vector2.Distance(transform.position, other.transform.position);
+
+                if (distance <= otherCircle.radius)
+                {
+                    if (other.TryGetComponent<PlacedLoop>(out var loop))
+                    {
+                        SwapToOnLoop(loop);
+                        return;
+                    }
+                }
+            }
+        }
+
         void Update()
         {
+            if (IsOnLoop == false)
+                return;
 
+            float currentLoopSpace = _currentLoop.PositionToLoopSpace(transform.position);
+            float speedPerFrame = _speed * Time.deltaTime;
+
+            float newLoopSpace = ((currentLoopSpace + (speedPerFrame / _currentLoop.Circumference)) + 100) % 1;
+            Vector3 newPos = _currentLoop.LoopSpaceToPosition(newLoopSpace);
+            newPos.z = transform.position.z;
+            transform.position = newPos;
         }
     }
 }
