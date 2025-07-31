479b341c9d3d44d1666c5ecb54f8bf69d15dd601
JoeShanahan <iam3djoe@gmail.com>
2025-07-31 11:14:42 +0100

    binary connector

---
+++ Assets/Scripts/Connectors/BinarySwapper.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Gmtk2025.Connectors
+{
+    public class BinarySwapper : Connector
+    {
+        [SerializeField] private SpriteRenderer _sprite;
+        [SerializeField] private Color _goColor;
+        [SerializeField] private Color _stopColor;
+        [SerializeField] private bool _canGo = true;
+
+        private void Start()
+        {
+            SetColor();
+        }
+
+        private void SetColor()
+        {
+            _sprite.color = _canGo ? _goColor : _stopColor;
+        }
+        
+        public override void OnProjectilePassed(Projectile projectile, PlacedLoop currentLoop)
+        {
+            if (_canGo)
+            {
+                PlacedLoop toLoop = currentLoop == _firstLoop ? _attachedLoop : _firstLoop;
+                projectile.SwapBetweenLoops(currentLoop, toLoop);
+            }
+
+            _canGo = !_canGo;
+            SetColor();
+        }
+    }
+}
