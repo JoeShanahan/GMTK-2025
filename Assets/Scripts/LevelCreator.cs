c61c09cfd20605003952416ed5fd6e7dbca5c43b
JoeShanahan <iam3djoe@gmail.com>
2025-08-01 10:48:03 +0100

    selector buttons

---
+++ Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Gmtk2025
+{
+    public class LevelCreator : MonoBehaviour
+    {
+        public void StartPlacingLoop(float radius)
+        {
+            
+        }
+
+        public void StartPlacingConnector(ConnectorType type, int value)
+        {
+            
+        }
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
